4a63856691c90c1220dfac749a59082dedb91e76
Chris Tippett <c.tippett@gmail.com>
2018-08-04 19:20:04 +1000

    Set default date filters for CLI

--- src/timepro_utils/cli.py
+++ src/timepro_utils/cli.py
@@ -1,9 +1,14 @@
 import sys
 import argparse
+from datetime import date
 
 from dateutil.parser import parse as dateparser
+from dateutil.relativedelta import relativedelta, MO, FR
 
-from .api import TimesheetAPI
+# from .api import TimesheetAPI
+
+
+TODAY = date.today()
 
 
 class TimesheetCLI:
@@ -35,21 +40,33 @@ class TimesheetCLI:
         get_parameters = parser.add_argument_group('filter options')
         get_parameters.add_argument('--start', dest='start_date', metavar='START_DATE', help='Start date of timesheet period')
         get_parameters.add_argument('--end', dest='end_date', metavar='END_DATE', help='End date of timesheet period')
+        get_parameters.add_argument('--week', dest='get_week', action='store_true', help="Get current week's timesheet")
+        get_parameters.add_argument('--month', dest='get_month', action='store_true', help="Get current month's timesheet")
         args = parser.parse_args(sys.argv[2:])
         if args.start_date and args.end_date:
-            date_kwargs = {
-                'start_date': dateparser(args.start_date),
-                'end_date': dateparser(args.end_date)
-            }
+            start_date = dateparser(args.start_date)
+            end_date = dateparser(args.end_date)
+        elif args.get_month:
+            start_date = TODAY + relativedelta(day=1)
+            end_date = TODAY + relativedelta(day=31)
+        elif args.get_week:
+            start_date = TODAY + relativedelta(weekday=MO(-1))
+            end_date = TODAY + relativedelta(weekday=FR)
         else:
-            date_kwargs = {}
-        api = TimesheetAPI()
-        api.login(
-            customer_id=args.customer_id,
-            username=args.username,
-            password=args.password)
-        timesheet = api.get_timesheet(**date_kwargs)
-        print(timesheet.json())
+            # default to get this week's timesheet (excl. previous month)
+            start_date = max([
+                TODAY + relativedelta(day=1),
+                TODAY + relativedelta(weekday=MO(-1))
+            ])
+            end_date = TODAY + relativedelta(weekday=FR)
+        date_kwargs = dict(start_date=start_date, end_date=end_date)
+        # api = TimesheetAPI()
+        # api.login(
+        #     customer_id=args.customer_id,
+        #     username=args.username,
+        #     password=args.password)
+        # timesheet = api.get_timesheet(**date_kwargs)
+        # print(timesheet.json())
 
 
 if __name__ == '__main__':
