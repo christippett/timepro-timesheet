d2646f3dd7831344e5ee9952f96af9172fd77857
Chris Tippett <c.tippett@gmail.com>
2019-02-12 00:02:39 +1100

    Remove Python 2 support

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 LONG_DESCRIPTION = open('README.md').read()
 
@@ -31,7 +30,6 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
